ce7bddf309f8dc74e6d6cbcaeb0290f014e6c0ab
Jaren-25 <ree25004@byui.edu>
2026-01-15 14:31:51 -0700

    Added mass conversion

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,13 +8,32 @@ namespace Sandbox
         {
             //Console.WriteLine("Hello World");
 
-            int x = 5;
-            // int, string, bool, double
-            Console.WriteLine("value of x is " + x);
+            // int x = 5;
+            // // int, string, bool, double
+            // Console.WriteLine("value of x is " + x);
 
-            Console.Write("question");
-            string color = Console.ReadLine();
+            // Console.Write("question");
+            // string color = Console.ReadLine();
 
+        Console.Write("Input weight: ");
+        string userInput = Console.ReadLine();
+        float weight = float.Parse(userInput);
+
+        Console.Write("Input 'LBS' or 'KG'");
+        string unit = Console.ReadLine();
+
+
+
+        if (unit == "LBS")
+            {
+                float kilo = weight / 2.205f;
+                Console.WriteLine(kilo);
+            }
+            else
+            {
+                float pound = weight * 2.205f;
+                Console.WriteLine(pound);
+            }
         }
     }
 }
