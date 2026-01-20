3487d353171968c4753b1960097ce06ad5590f15
Jaren-25 <ree25004@byui.edu>
2026-01-20 16:33:52 -0700

    Wrote thr program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+       int guess = 0;
+
+        while (magicNumber != guess)
+        {
+            // Console.Write("Type a magic number: ");
+            // string magicNumberInput = Console.ReadLine();
+            // magicNumber = int.Parse(magicNumberInput);
+
+
+            Console.Write("Enter a guess: ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicNumber == guess)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
