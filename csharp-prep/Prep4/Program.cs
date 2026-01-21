318de3f35fd7e290e82afcd8b9196c801d847ed6
Jaren-25 <ree25004@byui.edu>
2026-01-20 17:32:00 -0700

    Did the assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbersList = new List<int>();
+
+        int numbers = -1;
+
+        while (numbers != 0)
+        {
+            Console.Write("Input a number: ");
+            string numbersInput = Console.ReadLine();
+            numbers = int.Parse(numbersInput);
+
+            if (numbers != 0)
+            {
+                numbersList.Add(numbers);
+            }
+
+
+        }
+        int total = 0;
+        double average = 0;
+        int large = 0;
+
+        foreach (int number in numbersList)
+        {
+            //Console.WriteLine(number);
+            total = total + number;
+            average = (double)total / numbersList.Count;
+            if (number > large)
+            {
+                large = number;
+            }
+        }
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {large}");
+
     }
 }
