f829407f987d51c3bf13ae4860a14db0fa56399c
Jaren-25 <ree25004@byui.edu>
2026-01-23 18:08:58 -0700

    challange day

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,6 +6,51 @@ namespace Sandbox
     {
         public static void Main()
         {
+            // int i = 0;
+            // while(i < 5)
+            // {
+            //     Console.WriteLine("Hello World");
+            //     i ++;
+            // }
+
+            // Console.WriteLine("Enter a number: ");
+            // string numberInput = Console.ReadLine();
+            // int number = int.Parse(numberInput);
+            // int i = 0;
+
+            // while (i <= 12)
+            // {
+            //     Console.WriteLine(number * i);
+            //     i ++;
+            // }
+
+            // List<string>names = new List<string>{"bob", "susie", "fred", "Elsie"};
+
+            // Console.Write("Enter a name: ");
+            // string nameInput = Console.ReadLine();
+
+            // if(names.Contains(nameInput))
+            //     {
+            //         Console.WriteLine("That's in the list!");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("That's not in the list");
+            //     }
+
+    static string TitleCase(string sentence)
+            {
+                string upper = sentence.ToUpper();
+                //chara = sentence.ToCharArray();
+                return upper;
+            }
+
+            Console.Write("Enter a sentence: ");
+            string sentence = Console.ReadLine();
+            string upper = TitleCase(sentence);
+            Console.WriteLine(upper);
+
+
             //Console.WriteLine("Hello World");
 
             // int x = 5;
@@ -15,25 +60,27 @@ namespace Sandbox
             // Console.Write("question");
             // string color = Console.ReadLine();
 
-        Console.Write("Input weight: ");
-        string userInput = Console.ReadLine();
-        float weight = float.Parse(userInput);
+        // Console.Write("Input weight: ");
+        // string userInput = Console.ReadLine();
+        // float weight = float.Parse(userInput);
 
-        Console.Write("Input 'LBS' or 'KG'");
-        string unit = Console.ReadLine();
+        // Console.Write("Input 'LBS' or 'KG'");
+        // string unit = Console.ReadLine();
 
 
 
-        if (unit == "LBS")
-            {
-                float kilo = weight / 2.205f;
-                Console.WriteLine(kilo);
-            }
-            else
-            {
-                float pound = weight * 2.205f;
-                Console.WriteLine(pound);
-            }
+        // if (unit == "LBS")
+        //     {
+        //         float kilo = weight / 2.205f;
+        //         Console.WriteLine(kilo);
+        //     }
+        //     else
+        //     {
+        //         float pound = weight * 2.205f;
+        //         Console.WriteLine(pound);
+
+            // }
+
         }
     }
 }
