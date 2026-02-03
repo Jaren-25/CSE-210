1d763b9569f2727cace3f2cababf1be35d3789dc
Jaren-25 <ree25004@byui.edu>
2026-02-03 16:24:40 -0700

    car alarm sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,20 +6,23 @@ namespace Sandbox
     {
         public static void Main()
         {
+            Car myCar = new Car();
+            myCar.Run();
+
             // Console.WriteLine("Hello World");
 
             // Counter myCounter = new Counter();
             // myCounter.Run();
 
-            Message m1 = new Message();
-            m1._message = "Send help!";
-            m1._priority = 1;
-            Console.WriteLine(m1.GetMessage());
+            // Message m1 = new Message();
+            // m1._message = "Send help!";
+            // m1._priority = 1;
+            // Console.WriteLine(m1.GetMessage());
 
-            Message m2 = new Message();
-            m2._message = "Pick up carrots";
-            m2._priority = 3;
-            Console.WriteLine(m2.GetMessage());
+            // Message m2 = new Message();
+            // m2._message = "Pick up carrots";
+            // m2._priority = 3;
+            // Console.WriteLine(m2.GetMessage());
 
             // int i = 0;
             // while(i < 5)
