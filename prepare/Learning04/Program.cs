dfc560f0c762a8adef36a28335e8a3568e09c46c
Jaren-25 <ree25004@byui.edu>
2026-02-24 17:10:21 -0700

    wrote the code

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+
+        Assignment a1 = new Assignment("hehe", "calc");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment assignment = new MathAssignment("bob", "Adding", "7.3", "8-19");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine(assignment.GetHomeWorkList());
+
+
+        WritingAssignment a3 = new WritingAssignment("linda","topic", "title");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetwritingInformation());
     }
 }
