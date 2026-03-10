c966ba085ce21d06846910f3cb3c9b98d92cb525
Jaren-25 <ree25004@byui.edu>
2026-03-10 13:50:27 -0600

    wrote it

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //Console.WriteLine("Hello Learning05 World!");
+
+
+        Square square = new Square("blue", 4);
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("red", 2, 3);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("purple", 1);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 3));
+        shapes.Add(new Rectangle("orange", 3, 8));
+        shapes.Add(new Circle("maroon", 1));
+
+        foreach(Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
+
+
     }
 }
